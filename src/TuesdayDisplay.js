fc2dcd56ebace2ca070d91ae4c71b0cacc047f72
Mickey Poblete <mikhael.poblete@gmail.com>
2020-10-07 16:00:07 -0700

    trying to add a picture of a taco for tuesdays

--- src/TuesdayDisplay.js
+++ src/TuesdayDisplay.js
@@ -1,9 +1,10 @@
 import React from "react";
+import TacoPic from "./images/unsplashTaco.jpg";
 
 const TacoDisplay = (props) => {
-    console.log(props.today)
     return (
         <h1 style={{color:'red',backgroundColor:'yellow'}}>It's TACO TUESDAY!</h1>
+        <img src={TacoPic} alt="tacopic" /> //adding a picture of a taco
     );
 };
 
